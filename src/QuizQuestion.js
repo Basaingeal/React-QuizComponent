146a8a4e8df7e395b60cfa46570a02447acc2c1b
Michael J. Currie <mcurrie@agwsinc.com>
2020-01-15 15:44:31 -0600

    set state correctly

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -9,10 +9,10 @@ class QuizQuestion extends Component {
 
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
-      this.state.incorrectAnswer = false;
+      this.setState({ incorrectAnswer: false })
       this.props.showNextQuestionHandler();
     } else {
-      this.state.incorrectAnswer = true;
+      this.setState({ incorrectAnswer: true })
     }
   }
 
